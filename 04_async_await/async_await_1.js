1db1d65faa814d08a089f464623fb867947142bd
Mrinalini Upadhya <upadhya.mrinalini@gmail.com>
2020-04-27 11:46:16 -0700

    commit

---
+++ 04_async_await/async_await_1.js
@@ -0,0 +1,52 @@
+/* Think of this as syntactical sugar that is available
+in order to write these async code using promises in a sync fashion.
+Note that async await is built on top of promises.
+ */
+/* Below code re writes promise_2.js using async await, note the try catch
+is used in order to handle the .catch in promises */
+async function displayCommits(id) {
+  try {
+    const user = await getUser(id); // await waits for the async task to complete
+    const repos = await getRepositories(user.gitHubUserName);
+    const commits = await getCommits(repos[0]);
+    // displayCommits(commits); not required just do console log
+    console.log(commits);
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+/* ------------------------------------------------------- */
+/* Call the above function */
+/* ------------------------------------------------------- */
+displayCommits(20);
+console.log(
+  "Even though this line is written after displayCommits it will print before all these async task. This is a sync task"
+);
+/* ------------------------------------------------------- */
+/* replaced all callbacks to promises */
+/* ------------------------------------------------------- */
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Reading from database");
+      resolve({ id: id, gitHubUserName: "mrinalini" });
+    }, 2000);
+  });
+}
+function getRepositories(username) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(`Getting repo for ${username}`);
+      resolve(["repo1", "repo2"]);
+    }, 3000);
+  });
+}
+function getCommits(repo) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(`Getting commits for ${repo}`);
+      resolve(["commit1", "commit2", "commit3"]);
+    }, 3000);
+  });
+}
+/* ------------------------------------------------------- */
